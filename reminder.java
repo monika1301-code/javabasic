1bf78f261ab35542eaafc8c56bc9a0c90624420b
monika1301-code <sharma.monika13005@gmail.com>
2025-08-03 18:51:54 +0530

    reminder find

---
+++ reminder.java
@@ -0,0 +1,7 @@
+public class Remainder {
+  public static void main(String[] args) {
+    int dividend = 29, divisor = 5;
+    int remainder = dividend % divisor;
+    System.out.println("Remainder: " + remainder);
+  }
+}
